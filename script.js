c30649c58a1f783e70d918b5e606def336aae1c1
analebyodkina <dp030992cad@gmail.com>
2023-02-18 15:56:31 +0200

    init new repository and added files

---
+++ script.js
@@ -0,0 +1,7 @@
+// nav toogle
+function navToggle() {
+    var navToggleBtn = document.getElementById("heartIcon");
+    navToggleBtn.classList.toggle("active");
+    var navToggleBtn = document.getElementById("heartBlack");
+    navToggleBtn.classList.toggle("active");
+  }
