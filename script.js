7b7c25912a480899413269fab963617898478bee
analebyodkina <dp030992cad@gmail.com>
2023-02-19 20:59:10 +0200

    started to work an adaptivity

--- script.js
+++ script.js
@@ -4,4 +4,22 @@ function navToggle() {
     navToggleBtn.classList.toggle("active");
     var navToggleBtn = document.getElementById("heartBlack");
     navToggleBtn.classList.toggle("active");
+}
+function naToggle() {
+    var navToggleBtn = document.getElementById("heartIcon1");
+    navToggleBtn.classList.toggle("active");
+    var navToggleBtn = document.getElementById("heartBlack1");
+    navToggleBtn.classList.toggle("active");
+  }
+  function nToggle() {
+    var navToggleBtn = document.getElementById("heartIcon2");
+    navToggleBtn.classList.toggle("active");
+    var navToggleBtn = document.getElementById("heartBlack2");
+    navToggleBtn.classList.toggle("active");
+  }
+  function aToggle() {
+    var navToggleBtn = document.getElementById("heartIcon3");
+    navToggleBtn.classList.toggle("active");
+    var navToggleBtn = document.getElementById("heartBlack3");
+    navToggleBtn.classList.toggle("active");
   }
